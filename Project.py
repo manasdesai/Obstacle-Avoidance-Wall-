e7ae2d9e0258f4de4bb41591ff758eefa77eae40
manasdesai <62495882+manasdesai@users.noreply.github.com>
2021-06-26 00:03:36 +0530

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import rospy
+from sensor_msgs import Laserscan
+from geometry_msgs.msg import Twist
+def callback(msg):
+    if msg.ranges[360]>1:
+        move.linear.x=.5
+        move.angular.z=0.0
+    if msg.ranges[360]<1:
+        move.linear.x=0
+        move.angular.z=0
+    pub.publish(move)
+    
+rospy.init_node('moverobot')
+pub=rospy.Publisher('cmd_vel',Twist,queue_size=1)
+sub=rospy.Subscriber('/scan',LaserScan,callback)
+move=Twist()
+
+rospy.spin()
